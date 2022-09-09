674e900a3a76be499217dca83a3f2312a799cf6a
Hoang <jasonkindo1710@gmail.com>
2022-09-09 10:23:58 +0700

    button

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,21 +1,29 @@
-import { Typography } from "@material-ui/core";
+import { Typography, Button, ButtonGroup, Container } from "@material-ui/core";
+
 import React from "react";
 
 export default function Create() {
   return (
-    <div>
-      <Typography variant="h1" color="primary" align="center">
+    <Container>
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
         Create a new note
       </Typography>
-      <Typography color="secondary">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        vehicula sed lectus non mollis. Fusce sit amet molestie ligula. Ut id
-        libero turpis. Mauris bibendum, nibh sit amet elementum maximus, justo
-        nisl ornare libero, nec cursus felis libero nec neque.
-      </Typography>
-      <Typography variant="h6" component="h2" color="textSecondary" gutterBottom>
-        This is a new note
-      </Typography>
-    </div>
+
+      <Button type="submit">Submit</Button>
+      <Button type="submit" color="secondary" variant="outlined">
+        Submit
+      </Button>
+
+      <ButtonGroup color="secondary" variant="contained">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup>
+    </Container>
   );
 }
