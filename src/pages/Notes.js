5763c9a01d3aec9a4d4647961d75418fcc56fb10
Hoang <jasonkindo1710@gmail.com>
2022-09-09 14:58:43 +0700

    grid

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,4 +1,6 @@
+import { Grid, Paper } from '@material-ui/core'
 import React, { useEffect, useState } from 'react'
+
 
 export default function Notes() {
   const [notes, setNotes] = useState([])
@@ -11,9 +13,20 @@ export default function Notes() {
 
   return (
     <div>
-      {notes.map(note => (
-        <p key={note.id}>{note.title}</p>
-      ))}
+      <Grid container>
+        <Grid item xs={12} md={3}>
+          <Paper>1</Paper>
+        </Grid>
+        <Grid item xs={12} md={3}> 
+          <Paper>2</Paper>
+        </Grid>
+        <Grid item xs={12} md={3}>
+          <Paper>3</Paper>
+        </Grid>
+        <Grid item xs={12} md={3}>
+          <Paper>4</Paper>
+        </Grid>
+      </Grid>
     </div>
   )
 }
