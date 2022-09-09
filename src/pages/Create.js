9f660cbe721e6a99c5bb987a3d0fdffa23472921
Hoang <jasonkindo1710@gmail.com>
2022-09-09 10:14:18 +0700

    Typography

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Typography } from "@material-ui/core";
+import React from "react";
 
 export default function Create() {
   return (
     <div>
-      Create page
+      <Typography variant="h1" color="primary" align="center">
+        Create a new note
+      </Typography>
+      <Typography color="secondary">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        vehicula sed lectus non mollis. Fusce sit amet molestie ligula. Ut id
+        libero turpis. Mauris bibendum, nibh sit amet elementum maximus, justo
+        nisl ornare libero, nec cursus felis libero nec neque.
+      </Typography>
+      <Typography variant="h6" component="h2" color="textSecondary" gutterBottom>
+        This is a new note
+      </Typography>
     </div>
-  )
+  );
 }
